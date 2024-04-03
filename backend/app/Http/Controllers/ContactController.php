8add6d22121525d4ec3e08e48c56a8f4f8df9742
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2024-04-03 22:21:19 +0200

    added search on all columns and sorting for contacts

---
+++ backend/app/Http/Controllers/ContactController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contact;
+use App\Events\testWebsocket;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Contact::query();
+
+        // Sort by column
+        if ($request->has('sort')) {
+            $sortColumn = $request->input('sort');
+            $sortDirection = $request->input('direction', 'asc');
+
+            $query->orderBy($sortColumn, $sortDirection);
+        }
+
+        // Advanced search
+        if ($request->has('search')) {
+            $searchTerm = $request->input('search');
+            $query->where(function($q) use ($searchTerm) {
+                $q->where('name', 'like', "%$searchTerm%")
+                    ->orWhere('surname', 'like', "%$searchTerm%")
+                    ->orWhere('email', 'like', "%$searchTerm%")
+                    ->orWhere('number', 'like', "%$searchTerm%")
+                    ->orWhere('social_media_url', 'like', "%$searchTerm%")
+                    ->orWhere('date_of_birth', 'like', "%$searchTerm%")
+                    ->orWhere('note', 'like', "%$searchTerm%");
+            });
+        }
+
+        $contacts = $query->paginate(4);
+        event( new testWebsocket);
+        return view('contacts.index', compact('contacts'));
+    }
+}
--- backend/app/User.php
+++ backend/app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'is_admin', 'number', 'date_of_birth', 'social_media_url'
     ];
 
     /**
