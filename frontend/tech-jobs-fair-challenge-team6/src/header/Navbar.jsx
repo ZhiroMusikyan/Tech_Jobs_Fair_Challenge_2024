44b4ffe36f39fde6e6b6d8db7501137a8e8a80d6
Smau <smaudaniel19@gmail.com>
2024-04-04 13:23:01 +0200

    implements contact form modal

--- frontend/tech-jobs-fair-challenge-team6/src/header/Navbar.jsx
+++ frontend/tech-jobs-fair-challenge-team6/src/header/Navbar.jsx
@@ -3,6 +3,7 @@ import { SearchOutlined } from '@ant-design/icons';
 import './navbar.css';
 import { Header } from 'antd/es/layout/layout';
 import {  useState } from 'react';
+import ContactForm from '../components/contact_form/contact_form';
 
 
 const Navbar = ({ handleSetSearch = () => { } }) => {
@@ -12,6 +13,17 @@ const Navbar = ({ handleSetSearch = () => { } }) => {
     const { value } = e.target;
     setSearchQuery(value);
     handleSetSearch()
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
   };
 
   return <Header
@@ -30,7 +42,8 @@ const Navbar = ({ handleSetSearch = () => { } }) => {
         prefix={<SearchOutlined />}
       />
     </div>
-    <Button className='add-new-contact-button' type="primary">Add new contact</Button>
+    <Button onClick={showModal} className='add-new-contact-button' type="primary">Add new contact</Button>
+    <ContactForm open={isModalOpen} onConfirm={handleOk} onCancel={handleCancel}></ContactForm>
   </Header>
 }
 export default Navbar
