11dc5adcf4a90fe3e3e1513baea20bfd649e1e13
Smau <smaudaniel19@gmail.com>
2024-04-05 13:40:29 +0200

    adds success and error messages to contact form

--- frontend/tech-jobs-fair-challenge-team6/src/components/contact_form/contact_form.jsx
+++ frontend/tech-jobs-fair-challenge-team6/src/components/contact_form/contact_form.jsx
@@ -1,18 +1,34 @@
-import React from "react";
-import { Button, Form, Modal, Tabs } from "antd";
+import React, { useState } from "react";
+import { Button, Form, Modal, Tabs, message } from "antd";
 import BasicFields from "./components/basic_fields";
 import AdvancedFields from "./components/advanced_fields";
-
 const ContactForm = ({ open, onConfirm, onCancel }) => {
   const [form] = Form.useForm();
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const showSuccessMsg = () => {
+    messageApi.open({
+      type: "success",
+      content: "The contact was added successfully!",
+    });
+  };
+
+  const showErrorMsg = () => {
+    messageApi.open({
+      type: "error",
+      content: "Ups! Something went wrong, try to check the values entered.",
+    });
+  };
 
   const onFinish = (values) => {
     form.resetFields();
     console.log("Success:", values);
+    showSuccessMsg();
     onConfirm();
   };
   const onFinishFailed = (errorInfo) => {
-    console.log("Failed:", errorInfo);
+    console.log(errorInfo);
+    showErrorMsg();
   };
 
   const items = [
@@ -29,24 +45,27 @@ const ContactForm = ({ open, onConfirm, onCancel }) => {
   ];
 
   return (
-    <Modal title="New contact" open={open} onCancel={onCancel} footer={null}>
-      <Form
-        name="basic"
-        layout="vertical"
-        form={form}
-        initialValues={{ prefix: "1" }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Tabs defaultActiveKey="1" items={items} />
-        <Form.Item style={{ display: "flex", justifyContent: "end" }}>
-          <Button type="primary" htmlType="submit">
-            Add
-          </Button>
-        </Form.Item>
-      </Form>
-    </Modal>
+    <>
+      {contextHolder}
+      <Modal title="New contact" open={open} onCancel={onCancel} footer={null}>
+        <Form
+          name="basic"
+          layout="vertical"
+          form={form}
+          initialValues={{ prefix: "1" }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Tabs defaultActiveKey="1" items={items} />
+          <Form.Item style={{ display: "flex", justifyContent: "end" }}>
+            <Button type="primary" htmlType="submit">
+              Add
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
   );
 };
 export default ContactForm;
