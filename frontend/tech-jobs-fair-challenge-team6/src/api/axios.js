cfa71896ce19b538c467510c159b1a21a3b5585a
ZhiroMusikyan <jiromusikyan@gmail.com>
2024-04-05 08:57:09 +0200

    added useQuery, seted up axios, added notification

---
+++ frontend/tech-jobs-fair-challenge-team6/src/api/axios.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    // config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
