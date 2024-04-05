04b241dbe178f74d1cec6d1e07271fe11590157c
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2024-04-05 22:12:19 +0200

    add login logout

---
+++ backend/app/Http/Controllers/LoginController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed
+            return response()->json(['message' => 'Authenticated']);
+        }
+
+        // Authentication failed
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return response()->json(['message' => 'Logged out successfully']);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware(['auth'])->group(function () {
+//Route::group(['middleware' => ['auth']], function () {
+});
+    Route::resource("/contacts", ContactController::class);
+
+    Route::post('/login', 'LoginController@login')->name('login');
+    Route::post('/logout', 'LoginController@logout');
+
