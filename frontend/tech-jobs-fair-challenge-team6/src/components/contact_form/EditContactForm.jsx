3a8564ca458568a6448a095644d9258c9113430c
Smau <smaudaniel19@gmail.com>
2024-04-06 13:53:40 +0200

    adds edit contact form

---
+++ frontend/tech-jobs-fair-challenge-team6/src/components/contact_form/EditContactForm.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import BasicFields from "./components/BasicFields";
+import AdvancedFields from "./components/AdvancedFields";
+import { errorMessage, successMessage } from "./constants/messages";
+import TabForm from "../tab_form/TabForm";
+
+const EditContactForm = ({ data, onConfirm, onCancel }) => {
+  const items = [
+    {
+      key: "1",
+      label: "Basic info",
+      children: <BasicFields></BasicFields>,
+    },
+    {
+      key: "2",
+      label: "More info",
+      children: <AdvancedFields></AdvancedFields>,
+    },
+  ];
+
+  return (
+    <TabForm
+      title="Edit contact"
+      confirmValue="Edit"
+      items={items}
+      successMsg={successMessage}
+      errorMsg={errorMessage}
+      onConfirm={onConfirm}
+      onCancel={onCancel}
+      open={data !== undefined}
+      initialValues={data}
+    />
+  );
+};
+export default EditContactForm;
