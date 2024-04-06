88955a406eca5b0971b11abfb3f5b49be944f719
Kozy-Korpesh Tolep <kozykorpesh.tolep@nu.edu.kz>
2024-04-06 12:47:10 +0200

    added user and admin in UserTableSeeder.php

--- backend/database/seeds/UserTableSeeder.php
+++ backend/database/seeds/UserTableSeeder.php
@@ -16,7 +16,16 @@ class UserTableSeeder extends Seeder
         $faker = Faker::create();
         User::create([
             'name' => $faker->userName,
-            'email' => $faker->unique()->safeEmail,
+            'email' => 'user@gmail.com',
+            'password' => bcrypt('password'), // Assuming you want to set a default password
+            'last_connected' => $faker->dateTimeBetween('-1 years', 'now'),
+            'created_on' => $faker->dateTimeBetween('-2 years', '-1 years'),
+            'is_admin' => 1
+        ]);
+
+        User::create([
+            'name' => $faker->userName,
+            'email' => 'admin@gmail.com',
             'password' => bcrypt('password'), // Assuming you want to set a default password
             'last_connected' => $faker->dateTimeBetween('-1 years', 'now'),
             'created_on' => $faker->dateTimeBetween('-2 years', '-1 years'),
