115d43348b02224ac18858a80579ab30109b6a7a
Alessio Buonomo <alessio@Alessio.PC>
2024-04-06 14:18:47 +0200

    Added DepartmentController and Departments migrations

---
+++ backend/app/Http/Controllers/DepartmentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DepartmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+			return response()->json(['departments'=>Department::all()], 200);
+    }
+}
